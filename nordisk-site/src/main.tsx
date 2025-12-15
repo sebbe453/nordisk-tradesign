759e89386c8bdf03d3c6afcf9682a21f71396eb3
Sebastian Fredriksson <carlseb.fredriksson@gmail.com>
2025-12-15 16:06:57 +0100

    Fix: Startfilen använder nu HomePage och ContactSection

--- nordisk-site/src/main.tsx
+++ nordisk-site/src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import HomePage from "./pages/HomePage";
+import ContactSection from "./components/ContactSection";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HomePage />
+    <main>
+      <HomePage />
+      <ContactSection />
+    </main>
   </React.StrictMode>
 );
