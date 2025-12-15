488f6a027ca40ca084ecc63d2fcec1673e10acd2
Sebastian Fredriksson <carlseb.fredriksson@gmail.com>
2025-12-15 16:25:40 +0100

    Fix: Korrigerade importsökväg till 3 nivåer upp (../../../) för att matcha rotkomponenter.

--- nordisk-site/src/main.tsx
+++ nordisk-site/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import HomePage from "./pages/HomePage";
-import ContactSection from "./components/ContactSection";
+import HomePage from "../../../HomePage.tsx";
+import ContactSection from "../../../ContactSection.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
